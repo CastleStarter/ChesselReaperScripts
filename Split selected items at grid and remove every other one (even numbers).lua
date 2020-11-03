c0a33d33c750693195bccc8aa50d3e886959bd7e
Chessel <ckgoodwin85@gmail.com>
2020-11-03 21:11:00 +0000

    Upload both od and even scripts

---
+++ Split selected items at grid and remove every other one (even numbers).lua
@@ -0,0 +1,26 @@
+-- Function: Split selected items at grid and delete every other resulting item 
+-- Do even numbered items 
+
+function main()
+local ParentTrack
+local SelItem
+local ItemCount
+
+if reaper.CountSelectedMediaItems(0) > 0 then -- If there are selected items 
+reaper.Main_OnCommandEx(40932, 0 , 0) -- Split items at grid 
+
+ItemCount = reaper.CountSelectedMediaItems(0) -- Count the number of selected items
+if ItemCount % 2 > 0 then
+ItemCount = ItemCount - 1 -- If we have an odd number of items then start from the penultimate item 
+end 
+
+for i = ItemCount-1,0, -2 do
+SelItem = reaper.GetSelectedMediaItem( 0, i )
+if SelItem then 
+ParentTrack = reaper.GetMediaItemTrack( SelItem )
+reaper.DeleteTrackMediaItem (ParentTrack, SelItem )
+end -- If SelItem 
+end -- end of for item for loop 
+end --If selected items 
+end
+main()
